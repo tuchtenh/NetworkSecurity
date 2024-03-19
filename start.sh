27d21439301ebdd5adec35c4837f5e3f9503a8bf
tuchtenh <50889613+tuchtenh@users.noreply.github.com>
2024-03-19 13:14:10 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Update package repositories
 sudo apt-get update
 
-# Install necessary packages for Ansible
 sudo apt-get install -y ansible sshpass
 
-# Display Ansible version
 ansible --version
 
 # Generate ssh key
+exec ssh-agent bash
+
 ssh-keygen
+
 
 # Copy ssh key to remote mashine
 ssh-copy-id user@destip
