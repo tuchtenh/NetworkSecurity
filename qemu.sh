ddc9e96c43fc2f77c6e631879f455ad778e7dd43
tuchtenh <50889613+tuchtenh@users.noreply.github.com>
2024-03-22 22:36:00 +0200

    Update qemu.sh

--- qemu.sh
+++ qemu.sh
@@ -5,14 +5,19 @@ sudo apt install -y qemu-system-x86 qemu-utils
 
 mkdir -p /opt/mikrotik
 cd /opt/mikrotik
-wget https://download.mikrotik.com/routeros/7.5/routeros-7.5.npk
-unzip chr-7.1beta6.img.zip
+
+wget https://download.mikrotik.com/routeros/7.5/chr-7.5.img.zip
+
+unzip chr-7.5.img.zip
+
 
 qemu-img create -f qcow2 hda.img 1G
 
-qemu-system-x86_64 -hda hda.img -cdrom routeros-7.5.npk -boot d -m 512 -net nic -net user -rtc base=localtime -no-reboot
+
+qemu-system-x86_64 -hda hda.img -cdrom chr-7.5.img -boot d -m 512 -net nic -net user -rtc base=localtime -no-reboot
+
 
 qemu-system-x86_64 -hda hda.img -m 512 -net nic -net user -rtc base=localtime
 
 
-rm routeros-7.5.npk
+rm chr-7.5.img.zip
